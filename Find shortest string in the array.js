14b3b5db67493ec6a67cf7e033644afffddc32dd
Bhavanipriy <kbhavanipriya@yahoo.com>
2022-01-10 00:37:11 -0500

    Exercise

--- Find shortest string in the array.js
+++ Find shortest string in the array.js
@@ -1,4 +1,18 @@
+//"Using the inbuild function reduce()
+ 
  let employees=['Balu','Lakshmi','Prem','Raj','Janani']
 var shortest_string = employees.reduce(function(a,b){return a.length <= b.length?a:b})
 
 console.log(shortest_string +' '+'is the shortest string in the array')
+
+//Using the function 
+let employees=['Balu','Lakshmi','Prem','Raj','Ja']
+ function shortestString(a){
+    let x=a[0];
+    for(i=0;i<a.length;i++){
+        if(a[i].length<x.length)
+            x=a[i];
+    }
+     return x  
+}
+console.log(shortestString(employees))
